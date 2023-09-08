28a8c279460214ea99705c8d5d88c78d8eac8a92
Bernd Prünster <bernd.pruenster@a-sit.at>
2023-09-08 11:51:45 +0200

    update conventions+workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,6 @@ pluginManagement {
             url = uri("https://raw.githubusercontent.com/a-sit-plus/gradle-conventions-plugin/mvn/repo")
             name = "aspConventions"
         }
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
         mavenCentral()
         gradlePluginPortal()
     }
