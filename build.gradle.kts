a0ec27d914f4db35ea429c3304cca9d13d3fd858
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-06-28 15:57:24 +0200

    dynamic kotlin versions for pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("io.kotest.multiplatform") version "6.0.0.M4" apply false //we're JVM-only so apply false. but we need the plugin in the classpath until I clean up the conventions plugins
-    kotlin("jvm") version "2.1.21" apply false
-    kotlin("plugin.serialization") version "2.1.21" apply false
+    kotlin("jvm") version (System.getenv("KOTLIN_VERSION_ENV")?.let { it.ifBlank { null } }
+        ?: libs.versions.kotlin.get()) apply false
+    kotlin("plugin.serialization") version (System.getenv("KOTLIN_VERSION_ENV")?.let { it.ifBlank { null } }
+        ?: libs.versions.kotlin.get()) apply false
     id("at.asitplus.gradle.conventions") version "20250628"
 }
 
